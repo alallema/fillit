c03a72f148c2bb707d884094d1ac3f98b2844241
Amelie LALLEMAND <alallema@e1r13p8.42.fr>
2015-12-12 14:11:06 +0100

    nouveau fichier decoupe

---
+++ srcs/ft_read_file.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_read_file.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alallema <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/12 14:01:39 by alallema          #+#    #+#             */
+/*   Updated: 2015/12/12 14:09:21 by alallema         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <libft.h>
+#include <main.h>
+
+void    ft_read_file(char *av)
+{
+	int     fd;
+	char    buf;
+	char    *tab;
+	int     i;
+	
+	i = 0;
+	fd = open(av, O_RDONLY);
+	if (fd == -1)
+		ft_putstr("invalid file");
+	while (read(fd, &buf, 1))
+	{
+		if (buf != '.' && buf != '#' && buf != '\n')
+			return ;
+		i++;
+	}
+	close(fd);
+	fd = open(av, O_RDONLY);
+	tab = (char *)malloc(sizeof(char) * i + 1);
+	while (read(fd, tab, (i + 1)))
+		ft_create_list(tab, i);
+	close(fd);
+}
