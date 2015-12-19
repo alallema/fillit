4cb8525ff9a26281562d3d9b192a8fd3fb352ee4
Amelie LALLEMAND <alallema@e1r11p9.42.fr>
2015-12-19 17:27:52 +0100

    main.h

--- incs/main.h
+++ incs/main.h
@@ -6,7 +6,7 @@
 /*   By: alallema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/09 13:12:20 by alallema          #+#    #+#             */
-/*   Updated: 2015/12/19 14:17:49 by schiad           ###   ########.fr       */
+/*   Updated: 2015/12/19 17:27:16 by alallema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ void					ft_print_result(char **tab);
 void					ft_free_square(char **tab);
 
 t_tetr					*ft_create_elem(int **pattern, char letter);
+void					ft_lst_pushback(t_tetr **begin_list, int **data);
 int						ft_lst_size(t_tetr *tetr);
 void					ft_print_list(t_tetr *tetr);
 t_tetr					*ft_create_list(char *s, int n);
