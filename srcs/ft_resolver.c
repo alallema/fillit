2dbe8072f66eebdd42c4f7cab3259bde1f424234
Salim CHIAD <schiad@e1r9p5.42.fr>
2016-01-07 11:18:28 +0100

    resolver a la norme

--- srcs/ft_resolver.c
+++ srcs/ft_resolver.c
@@ -6,7 +6,7 @@
 /*   By: schiad <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/12 16:05:09 by schiad            #+#    #+#             */
-/*   Updated: 2016/01/07 08:33:10 by schiad           ###   ########.fr       */
+/*   Updated: 2016/01/07 10:46:30 by schiad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,9 @@ void		ft_resolver(t_tetr *tetr, int nb)
 	while (retour && dots > ((i * 2) - 2))
 	{
 		i--;
-		ft_putnbr(i);
-		ft_putchar('\n');
 		ft_square_char_filler(tab, i);
 		retour = ft_placeur(tab, tetr, nb);
-		ft_print_result(tab);
 		dots = ft_nb_dots(tab);
-		ft_putnbr(dots);
-		ft_putchar('\n');
-		ft_putstr("end\n");
 	}
 	if (!retour)
 		i++;
